caca7d54b0d75f0b5378441f83873d9c0c7edb62
DaviLim <davilimasoares1@outlook.com>
2023-12-08 20:45:20 -0300

    wrapper

--- js/control.js
+++ js/control.js
@@ -1,11 +1,14 @@
 function token(limit) {
   let token = "";
-  let char =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
   for (let index = 0; index < limit; index++) {
     token += char.charAt(Math.floor(Math.random() * char.length));
-    console.log(token);
   }
 }
 token(20);
+
+const s = "Oi, Tudo bem?";
+const words = s.substring(s.indexOf(" ") + 1, s.length);
+
+console.log(`String: ${s} | Substring: ${words}`);
